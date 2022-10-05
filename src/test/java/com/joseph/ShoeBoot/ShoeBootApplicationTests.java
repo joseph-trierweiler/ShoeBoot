e2a3e42e652144a020390f522f52a809a0934b7b
Jojo <jotreewater@gmail.com>
2022-10-05 11:10:07 -0700

    initial commit

---
+++ src/test/java/com/joseph/ShoeBoot/ShoeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseph.ShoeBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
